204e25c2541a796a93b42ca89edfca0bd152c21d
delvin-fil <delvin.fil@gmail.com>
2018-04-13 14:46:47 +0700

    Add project

--- ocrgui.py
+++ ocrgui.py
@@ -21,7 +21,7 @@ warnings.filterwarnings("ignore")
 cwd = os.path.expanduser('~')
 dtout = ''
 filename = "ocr.ui"
-filename = f"{cwd}/codding/ocr/{filename}"
+filename = f"{cwd}/codding/ocr/{filename}" # Заменить на путь к репозитрию
 Form, Base = uic.loadUiType(filename)
 
 
@@ -37,7 +37,7 @@ class MyWindow(QtWidgets.QWidget, Form):
         self.ui.btnConv.setToolTip("Распознать")
         self.ui.showd.clicked.connect(self.getfiles)
         self.ui.btnSave.clicked.connect(self.save_f)
-        self.ui.showd.setToolTip("Открывает только PNG")
+        self.ui.showd.setToolTip("Открывает изображение")
         self.ui.mylabel_2.setText("Входное изображение")
         self.ui.btnConv.clicked.connect(self.ocr)
         self.ui.mylabel.setText(f" ")
@@ -49,7 +49,7 @@ class MyWindow(QtWidgets.QWidget, Form):
             QFileDialog(self, quit())
         with open(fname, 'rb') as data:
             self.ui.mylabel_2.setScaledContents(True)
-            self.ui.mylabel.setText(f"Input file: {fname}")
+            self.ui.mylabel.setText(f"Файл: {fname}")
             self.ui.mylabel_2.setPixmap(QtGui.QPixmap(f"{fname}"))
         print (f"\n{fname}")
         global fout
@@ -69,7 +69,7 @@ class MyWindow(QtWidgets.QWidget, Form):
             out  = self.ui.textOut.toPlainText()
             with open(fout, 'w') as f_out:
                 f_out.write(out + "\n")
-                self.ui.mylabel.setText('Saved file: ' + fout)
+                self.ui.mylabel.setText('Файл сохранен: ' + fout)
 
 finish = time.clock()
 itog = str(round(finish - start, 3))
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     pal = window.palette()
     window.setFixedSize(656, 320)
     window.setWindowTitle("Gui for tesseract")
-    window.setWindowIcon(QtGui.QIcon(f"{cwd}/fontmanager.gif"))
+    window.setWindowIcon(QtGui.QIcon(f"{cwd}/fontmanager.gif")) # Заменить на путь к иконке или комментировать
     window.show()
     sys.exit(app.exec_())
     
